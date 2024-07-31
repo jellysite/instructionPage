eb251c1b67e89e0df3865bed034617b29d555063
Leonie <leoniele1990@gmail.com>
2024-07-31 13:44:00 +0100

    Initial Commit

---
+++ Instructions/InstructionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstructionsApp.swift
+//  Instructions
+//
+//  Created by Scholar on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstructionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
